e39b5a7d6aa18bc3740f4188f1e71977458f7fa1
idealius <idealius@users.noreply.github.com>
2021-01-10 13:26:53 -0600

    generate curves disabled due to rework
    updated readme.md as well

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 ![Logo](/img/Spicy_Covid_Graphs_Logo.png)
 
-Link: (http://covid19graphs.42web.io/)
+Link: (http://covid19graphs.42web.io/index.html)
 
 
 ### Purpose:
-When doing personal risk assessment of COVID-19 in different contexts like local and global regions, other dashboards did not make it easy see regions overlayed and per population total. To rectify this would involve a lot of copy/pasting and data transposing in spreadsheets like Excel. The Python project listed in sources below is used to transpose Johns Hopkins data and convert it to rates. This makes the data easy to use in Tableau, Excel, etc.
+When doing personal risk assessment of COVID-19 in different contexts like local and global regions, other dashboards did not make it easy see regions overlayed and per population total with adjustable moving averages. To rectify this would involve a lot of copy/pasting and data transposing in spreadsheets like Excel. The Python project (also mine) listed in sources below is used to transpose Johns Hopkins data and convert it to rates. This makes the data easy to use in Tableau, Excel, etc.
 
 This project then takes that data outputted from the Python project (which also converts the data to .js files) and allows one to quickly overlay different regions - by user selection - or by ranking across N number of days for cases and deaths.
 
@@ -13,11 +13,11 @@ Always shows data as percentage of population, so comparisons between regions is
 
 Rolling average slider.
 
-Always fills curves transparently -- mostly for aesthetic reasons, but also because it is sometimes easier to visualize differences between regions.
+Filled curves option, sometimes better for detecting patterns.
 
 Totals printed to text area for ranking comparison across N number of days.
 
-Ranking in the graph by total or peak.
+Ranking in the graph by total, peak, or slope acrss N days.
 
 Ability to add N top regions in a dataset by last N number of days. For seeing, say the bottom 10 regions, one can add the top N regions, then remove the (N - 10) regions.
 
@@ -36,11 +36,11 @@ A. TBD
 12/1/2020: Mobile footer displays partly off the footer gradient
 
 ### Potential Enhancements
-Zoom out show letters for months instead of abbreviations
+Zoom out show letters for months instead of abbreviations - Added
 
 Find way to change selected region curve on top (JSXGraph feature / limitation)
 
-Add remove region by context functionality
+Add remove region by context functionality - Hide feature Added
 
 Add dual axis for easier comparison of deaths to cases
 
@@ -50,13 +50,11 @@ Add 'clear' button for text area
 
 Test more mobile screen rotations
 
-Add text box log for remove_top regions
-
 Note why some colors are outlined in mobile on main page
 
-Add Tabs
+Add Tabs or popup dialogs for more options
 
-Add curve guessing using logarthmic regression that has decay
+Add curve guessing using logarthmic regression that has decay - This was added, now being reworked to be multiple curves added together for one sum curve.
 
 Mobile zoom: find a way to consolidate separate vert, horiz, and diagonal two finger zooming (JSXGraph feature / limitation)
 
@@ -74,6 +72,11 @@ Graphs are not displayed well on mobile platforms in general because the screen 
 #### Third party software packages:
 
 This project uses JQuery (https://jquery.com/) and JSXGraph (https://jsxgraph.uni-bayreuth.de/wp/index.html) extensively 
+
+This project was helped tremendously by http://www.stackoverflow.com
+For the deprecated curve matching it uses a small project (fminsearch) from: https://github.com/jonasalmeida/fminsearch
+And, it may at some point include this small project which is an implementation of Newton's Method:
+https://gist.github.com/TragicSolitude/796f2a1725e9abf13638
 
 #### This uses my Python data transposing project
 
--- third_party/fminsearch.js
+++ third_party/fminsearch.js
@@ -1,4 +1,12 @@
-fminsearch=function(fun,Parm0,x,y,Opt){// fun = function(x,Parm)
+// MIT License
+// Copyright <YEAR> <COPYRIGHT HOLDER>
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+//Original code from https://github.com/jonasalmeida/fminsearch
+
+fminsearch=function(func,Parm0,x,y,Opt, func2){// fun = function(x,Parm)
 	// example
 	//
 	// x = [32,37,42,47,52,57,62,67,72,77,82,87,92];y=[749,1525,1947,2201,2380,2537,2671,2758,2803,2943,3007,2979,2992]
@@ -16,13 +24,13 @@ fminsearch=function(fun,Parm0,x,y,Opt){// fun = function(x,Parm)
 	if(!Opt.step){// initial step is 1/100 of initial value (remember not to use zero in Parm0)
 		Opt.step=Parm0.map(function(p){return p/100});
 		// inform(Opt.step);
-		Opt.step=Opt.step.map(function(si){if(si==0){return .0001}else{ return si}}); // convert null steps into 1's //Updated to .0001
+		Opt.step=Opt.step.map(function(si){if(si==0){return .0000000001}else{ return si}}); // convert null steps into 1's //Updated to .0001
 	};
 	if(typeof(Opt.display)=='undefined'){Opt.display=true};
 
 
 	if(!Opt.objFun) { //Cost function
-		Opt.objFun=function(y, yp) {
+		Opt.objfunc=function(y, yp) {
 			return y.map( function(yi, i) {
 				// inform(yi, yp);
 				return Math.pow((yi - yp[i]), 2)
@@ -39,37 +47,101 @@ fminsearch=function(fun,Parm0,x,y,Opt){// fun = function(x,Parm)
 	var n = P0.length;
 	var step=Opt.step;
 
-	var funParm = function(P) {
-		return Opt.objFun(y, fun(x,P));
+	var funcParm = function(P) {
+		if (Opt.step_style) {
+			var result = [];
+			for (var i = 0; i < x.length; i++) {
+				result.push(func(x[i],P));
+			}
+			return Opt.objfunc(y, result);
+		}
+		return Opt.objfunc(y, func(x,P));
 	}//function (of Parameters) to minimize
 	
+	// if (Opt.curves) inform(Opt.curves);
+
 	// silly multi-univariate screening
+	var skip = false;
 	for(var i=0;i<Opt.maxIter;i++){
 		
-		for(var j=0;j<n;j++){ // take a step for each parameter
-			
-			P1=cloneVector(P0);
-			P1[j]+=step[j];
-			// inform(funParm(P0), funParm(P1));
-			// if (funParm(P0) != funParm(P0)) {
-			// 	console.log("NaN!");
-			// 	return NaN;
-			// }
-			// else console.log("No NaN!!");
-			if(funParm(P1)<funParm(P0)){ // if parm value going in the right direction
-				step[j]=1.2*step[j]; // then go a little faster
-				P0=cloneVector(P1);
-				// console.log("getting closer");
+		if (Opt.reverse) {
+			// inform(fun(100,P0));
+			for(var j=n-1;j>-1;j--){ // take a step for each parameter
+
+				P1=cloneVector(P0);
+				P1[j]+=step[j];
+				if (Opt.func2 && j % 4 == 0) {
+					var index = Math.floor(j / 4);
+					var curve = Opt.curves[index];
+					curve.Parms = [P1[j], P1[j+1], P1[j+2], P1[j+3]];
+					if (Opt.func2(curve.x_offset, curve) > 0 || Opt.func2(curve.x_offset+x.length+20, curve) > 0) {
+						skip = true;
+						inform ("skipped");
+					}
+				}
+				// if (j % 2 == 1) // Looking for Exponents based on position in P
+				// {
+				// 	if (Math.abs(P1[j]) > 9) { //Exponents > 9 are ridiculous throw them out
+				// 		skip = true;
+				// 	}
+				// }
+				// else skip = false;
+				if(funcParm(P1)<funcParm(P0) && !skip){ // if parm value going in the right direction
+
+					step[j]=1.2*step[j]; // then go a little faster
+					P0=cloneVector(P1);
+					//inform(P1);
+					// console.log("getting closer");
+				}
+				else{
+					step[j]=-(0.5*step[j]); // otherwise reverse and go slower
+					// inform(j, step[j]);
+					skip = false;
+				}
+				// inform(funcParm(P1),funParm(P0));
 			}
-			else{
-				step[j]=-(0.5*step[j]); // otherwise reverse and go slower
-				// console.log(step[j]);
-			}	
 		}
-		if(Opt.display){if(i>(Opt.maxIter-10)){console.log(i+1,funParm(P0),P0)}}
+		else {
+			for(var j=0;j<n;j++){ // take a step for each parameter
+
+				P1=cloneVector(P0);
+				P1[j]+=step[j];
+				if (Opt.func2 && j % 4 == 0) {
+					var index = Math.floor(j / 4);
+					var curve = Opt.curves[index];
+					curve.Parms = [P1[j], P1[j+1], P1[j+2], P1[j+3]];
+					inform(Opt.func2(curve.x_offset, curve));
+					if (Opt.func2(curve.x_offset, curve) > 0 || Opt.func2(curve.x_offset+x.length+20, curve) > 0) {
+						skip = true;
+						inform ("skipped");
+					}
+				}
+				// if (j % 2 == 1) // Looking for Exponents based on position in P
+				// {
+				// 	if (Math.abs(P1[j]) > 9) { //Exponents > 9 are ridiculous throw them out
+				// 		skip = true;
+				// 	}
+				// }
+				// else skip = false;
+				if(funcParm(P1)<funcParm(P0) && !skip){ // if parm value going in the right direction
+
+					step[j]=1.2*step[j]; // then go a little faster
+					P0=cloneVector(P1);
+					//inform(P1);
+					// console.log("getting closer");
+				}
+				else{
+					step[j]=-(0.5*step[j]); // otherwise reverse and go slower
+					// inform(j, step[j]);
+					skip = false;
+				}
+				// inform(funcParm(P1),funParm(P0));
+			}
+		}
+		if(Opt.display){if(i>(Opt.maxIter-10)){console.log(i+1,funcParm(P0),P0)}}
 	}
 	if (!!document.getElementById('plot')){ // if there is then use it
-		fminsearch.plot(x,y,fun(x,P0),P0);
+		fminsearch.plot(x,y,func(x,P0),P0);
 	}
 	return P0
 };
